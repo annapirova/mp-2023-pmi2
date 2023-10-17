35c1bb42ecdb6db5d54b2c058eed87ba006b4648
Artem Barinov <artyom.barinov.2005@bk.ru>
2023-10-17 21:59:28 +0300

    Быки и коровы - почти работает

--- Artem_Barinov/№3.cpp
+++ Artem_Barinov/№3.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-int* num1(int a) {
+int* num1(int a) { // генерация числа длины А
 	srand(time(NULL));
 
 	int* N;
@@ -11,18 +11,36 @@ int* num1(int a) {
 		N[i] = rand() % 10 + 1;
 	return N;
 }
-void game(int* N, int a) {
-	printf("The game started!\nYou have to guess the numbers.\n");
-	
-	do{
-		int dig;
-		scanf_s("%", dig);
-		for (int i = 0; i < a; i++)
-		{
-			if (dig == N[i])
-				printf("Cow! You guess a digital with number %d\n", i);
-			else
-				printf("No matches! Try again!\n");
+void game(int* N, int a) {// игра
+	printf("The game started!\nYou have to guess the number.\n");
+	printf("Enter a number: ");
+
+	int arr[5];
+	int digC = 0;
+	for (int j = 0; j < 5; j++) {
+		arr[j] = dig % 10;
+		digC /= 10;
+	}
+
+	do{ //сама игра
+		unsigned int dig = 0;
+		if (scanf_s("%u", &dig)!=0) {
+			printf("Wrong simbol! Try again!\n");
+			rewind(stdin);
+			continue;
+		}
+		for (int i = a; i >= 0; i++) {
+			for (int k = 0; k < a; k++)	{
+				if (arr[k] == N[i])
+					printf("Cow! You guessed a digital of number: %d;\n", N[i]);
+			}
+			int f = i;
+			for (int k = 0; k < a; k++) {
+				if (arr[k] == N[f]) {
+					printf("Cow! You guessed a digital of number: %d;\n", N[i]);
+					f--;
+				}
+			}
 		}
 	} while (1);
 }
