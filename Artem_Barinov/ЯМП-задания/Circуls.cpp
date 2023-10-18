121aa5dfc633bbaf6be483d655b71d4a38339e2b
Artem Barinov <artyom.barinov.2005@bk.ru>
2023-10-18 11:29:30 +0300

    Окружности

--- Artem_Barinov/ЯМП-задания/Circуls.cpp
+++ Artem_Barinov/ЯМП-задания/Circуls.cpp
@@ -14,10 +14,34 @@ double DisFrCen(double x1, double y1,
 	double dis = sqrt(pow(x2-x1, 2)+pow(y2-y1, 2));
 	return dis;
 }
+double rad(double x1, double y1) {
+	double r = 0.0;
+	printf("Enter a value of radius: "); scanf_s("%lf", &r);
+	return r;
+}
+void output(double dis, double r1, double r2) {
+	double max = (r1 > r2) ? r1 : r2;
+	double min = r1 + r2 - max;
+	if (dis > r1 + r2)
+		printf("The circles do not intersect!\n");
+	if (dis == r1 + r2)
+		printf("The circles touch each other!\n");
+	if (dis < r1 + r2) {
+		if (dis + min == max)
+			printf("One circle includes another! And they touch each other!\n");
+		if (dis == 0)
+			printf("Circles have one center! And one circle includes another!\n");
+		else
+			printf("One circle includes another!\n");
+	}
+}
 int main() {
 	double x1 = circle('x', 1, 1); double y1 = circle('y', 1, 0); // vvod coordinat i raschet rasstoania
-	double x2 = circle('x', 1, 1); double y2 = circle('y', 1, 0);
+	double r1 = rad(x1, y1);										// i stitivaem radius
+	double x2 = circle('x', 1, 1); double y2 = circle('y', 1, 0);	
+	double r2 = rad(x2, y2);
+
 	double dis = DisFrCen(x1, y1, x2, y2);
-	printf("Distens: %lf\n", dis);
+	output(dis, r1, r2); // reshaem kak raspolozeni
 	return 0;
 }
