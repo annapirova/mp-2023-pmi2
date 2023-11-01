9d2196587cd98ffc80a5a9c1022e7baa2a89e52b
Artem Barinov <artyom.barinov.2005@bk.ru>
2023-11-01 14:04:31 +0300

    exercise29 - massive

--- Artem_Barinov/ЯМП-задания/mass 5-6.cpp
+++ Artem_Barinov/ЯМП-задания/mass 5-6.cpp
@@ -1,17 +1,17 @@
-#include <stdio.h>
-
-void output(int* arr, int len) { // vivod snachala chotnih potom nechetnih
-	for (int i = 0; i < len; i++) {(i % 2 == 0) ? printf("%d", arr[i]) : printf("  "); }
-	for (int i = 0; i < len; i++) { (i % 2 != 0) ? printf("%d", arr[i]) : printf("  ");	}
-}
-void locmax(int* arr, int len) {
-	for (int i = 0; i < len; i++) {
-		if (i == len - 2 && arr[i - 1] < arr[i] || arr[i] > arr[i + 1])
-			printf(" %d ", arr[i++]);
-	}
-}
-int main() {
-	int arr[10] = {0, 10, 1, 2, 3, 1, 4, 23, 8, 9}; int len = *(&arr + 1) - arr;
-	locmax(arr, len);
-	return 0;
-}
+//#include <stdio.h>
+//
+//void output(int* arr, int len) { // vivod snachala chotnih potom nechetnih
+//	for (int i = 0; i < len; i++) {(i % 2 == 0) ? printf("%d", arr[i]) : printf("  "); }
+//	for (int i = 0; i < len; i++) { (i % 2 != 0) ? printf("%d", arr[i]) : printf("  ");	}
+//}
+//void locmax(int* arr, int len) {
+//	for (int i = 0; i < len; i++) {
+//		if (i == len - 2 && arr[i - 1] < arr[i] || arr[i] > arr[i + 1])
+//			printf(" %d ", arr[i++]);
+//	}
+//}
+//int main() {
+//	int arr[10] = {0, 10, 1, 2, 3, 1, 4, 23, 8, 9}; int len = *(&arr + 1) - arr;
+//	locmax(arr, len);
+//	return 0;
+//}
