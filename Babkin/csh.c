5a3ab21b8ef7650122478c64fe2adac2294bdbcc
Unknown <144899753+KlozenSMA@users.noreply.github.com>
2023-11-22 02:11:45 +0300

    Касса?
    Переписал всю кассу. Я долго искал ошибки. Но сейчас она работает.

--- Babkin/csh.c
+++ Babkin/csh.c
@@ -3,89 +3,76 @@
 #include "stdbool.h"
 int main()
 {
-    int strhcodes[10] = {9241,4124,3313,8236,9102,6022,7247,4576,1000,6123};
-    double cost[10] = {25,19,61,24,90,123,119,51,33,516};
-    double discounts[10] = {0.23,0.45,0.15,0.20,0.09,0.1,0.08,0.05,0.07,0.25};
-    double costnew[10] = {0,0,0,0,0,0,0,0,0,0};
-    double discountsnew[10] = {0,0,0,0,0,0,0,0,0,0};
-    int chequelist[1000];
-    int chequelistcount[10];
-    int chequelistcorrect[10];
-    int a;
-    int alsum = 0;
-    int count = 0;
-    int count1 = 0;
-    int count2 = 0;
-    printf_s("Print 0 in codespace to finish. Remember, your bill cannot be empty.\nEnter the product code: ");
-    scanf_s("%d\n",&a); //По какой то неизвестной причине выходные значения терминала смещаются. Из-за чего возникают визуальные ошибки. Я не знаю как это править
-    while (a != 0)
+    int prcodes[10] = {2341,5130,1736,1334,9012,5136,9121,1000,7619,2280};
+    double price[10] = {231,351,102,412,90,10,67,190,61,345};
+    double discounts[10] = {0.19,0.1,0.06,0.12,0.3,0.32,0.09,0.21,0.1,0.12};
+    int productlist[10];
+    double pricelist[10];
+    int countlist[10];
+    double discountlist[10];
+    int input = 1;
+    int counter = 0;
+    int sum = 0;
+    printf("If you want to end shopping, please print 0\n");
+    while (input != 0)
     {
-        bool k9 = false;
+        int y = 0;
+        scanf("%d",&input);
+        printf("\n");
+        bool hmm = false;
+        bool what = true;
+        for (int cai = 0;cai < counter;cai++)
+        {
+            if (productlist[cai] == input)
+            {
+                y = 1;
+                break;
+            }
+        }
         for (int i = 0;i < 10;i++)
         {
-            if (a == strhcodes[i])
+            if ((prcodes[i] == input) && (y == 0))
             {
-                printf_s("About: \n");
-                printf_s("Strhcode: %d    Cost: %lf    Discount: %lf(in per cents)\n",strhcodes[i],cost[i],discounts[i]*100);
-                k9 = true;
-                chequelist[count] = strhcodes[i];
-                printf_s("Your cheque has been updated\n");
+                printf("scanning...\n");
+                productlist[counter] = input;
+                pricelist[counter] = price[i];
+                countlist[counter] = 1;
+                discountlist[counter] = discounts[i];
+                printf("You added %d to your cheque. Price: %lf . Discount: %lf per cents. \n",input,price[i],discounts[i]*100);
+                hmm = true;
+                counter ++;
             }
-        }
-        if (k9 == false)
-        {
-            printf("This product doesnt exist\n");
-        }
-        count += 1;
-        printf_s("Enter the product code: ");
-        scanf_s("%d\n",&a);
-    }
-    for (int p = 0;p < 1000;p++)
-    {
-        for (int g = 0;g < 10;g++)
-        {
-            if (chequelist[p] == strhcodes[g])
+            else
             {
-                for (int search = p + 1;search < 1000;search++)
+                if (y == 1)
                 {
-                    if (chequelist[search] == chequelist[p])
+                    for(int o = 0;o < counter;o++)
                     {
-                        chequelistcount[count1] += 1;
-                        chequelist[search] = 0;
+                        if (productlist[o] == input)
+                        {
+                            printf("scanned. You added another %d.",productlist[o]);
+                            countlist[o]++;
+                            hmm = true;
+                            what = false;
+                            break;
+                        }
                     }
                 }
-                count1 += 1;
             }
+            if (what == false)
+            {break;}
         }
+        if (hmm == false)
+        {printf("Your product doesnt exist\n");}
     }
-    for (int t = 0;t < 1000;t++)
+    printf("======== YOUR CHEQUE ========\n");
+    for (int p = 0;p < counter;p++)
     {
-        if (chequelist[t] != 0)
-        {
-            chequelistcorrect[count2] = chequelist[t];
-            count2 += 1;
-        }
+        printf("======\n");
+        printf("%d.\nProduct....%d\nOne cost....%lf\nAmount....%d\nAll cost....%lf\n",p+1,productlist[p],round(pricelist[p]*discountlist[p]),countlist[p],round(pricelist[p]*discountlist[p])*countlist[p]);
+        printf("======\n");
+        sum += round(pricelist[p]*discountlist[p])*countlist[p];
     }
-    for (int gh = 0;gh < 10;gh ++)
-    {
-        for (int sh = 0;sh < 10;sh ++)
-        {
-            if (chequelistcorrect[gh] == strhcodes[sh])
-            {
-                costnew[gh] = cost[sh];
-                discountsnew[gh] = discounts[sh];
-            }
-        }
-    }
-    printf_s("======== CHEQUE PRINTING ========\n");
-    printf_s("=== TO BE PAID ===\n");
-    for (int y = 0;y < count1;y++)
-    {
-        int kmit = y + 1;
-        printf_s("%d. %d. Cost for one: %d. Amount: %d. Cost for all %d\n",kmit,chequelistcorrect[y],round(costnew[y]*discountsnew[y]),chequelistcount[y],round(costnew[y]*discountsnew[y])*chequelistcount[y]);
-        alsum += round(costnew[y]*discountsnew[y])*chequelistcount[y];
-    }
-    printf_s("=======+ Total cost: %d +=======");
-    printf_s("Thank you for purchasing");
-    return 0;
+    printf("Conclusive summ: %d\n",sum);
+    printf("Thank you for using our virtual shop.");
 }
