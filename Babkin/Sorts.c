d21a135eb023cb5a64f0719bc43c0ec44e3d7efb
Даниил Бабкин <09savvy09@gmail.com>
2023-11-22 16:07:58 +0300

    New basis

---
+++ Babkin/Sorts.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "math.h"
+int main()
+{
+	int answq;
+	printf("Which sort do you want to start\n");
+	printf("1.Quick sort\n2.Shell's sort\n3.Choice sort\n4.Bubble sort\n5.Piramide sort\n6.Insert sort\n");
+	scanf_s("%d",&answq);
+}
