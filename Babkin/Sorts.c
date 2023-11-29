a23816ad9cbba9e2d139724e43a8c05ccd41018c
Unknown <144899753+KlozenSMA@users.noreply.github.com>
2023-11-29 16:05:23 +0300

    Added unoptimized bubble sort

--- Babkin/Sorts.c
+++ Babkin/Sorts.c
@@ -1,17 +1,44 @@
 #include "stdio.h"
 #include "math.h"
 #include "time.h"
-void Quicksort(int a[],int l,int r)
-{
-	int med = round((l+r) / 2);
-	int next = a[med];
-}
+void Quicksort()
+{}
 void Shellsort()
 {}
 void Choicesort()
 {}
-void Bubblesort()
-{}
+int Bubblesort(int a[],int l)
+{
+	int med = l / 2;
+	for (int o = 0; o < l; o++)
+	{
+		for (int i = 0; i < l - 1; i++)
+		{
+			if (a[i + 1] < a[i])
+			{
+				int tmp = a[i + 1];
+				a[i + 1] = a[i];
+				a[i] = tmp;
+			}
+		}
+	}
+	printf_s("Result: ");
+	for (int b = 0; b < l; b++)
+	{
+		printf_s("%d ",a[b]);
+	}
+	printf_s("\n");
+	return a;
+}
+void laws(int amg[],int l)
+{
+	double timex = 0.0;
+	clock_t begin = clock();
+	int amk = Bubblesort(amg, 12);
+	clock_t end = clock();
+	timex += (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time elapsed %lfs", timex);
+}
 void Piramidesort()
 {}
 void Insertsort()
@@ -19,36 +46,34 @@ void Insertsort()
 int main()
 {
 	int answq;
-	int array[12] = {8,12,78,65,1,23,53,11,90,45,100,123};
+	int array[12];
+	for (int i = 0; i < 12; i++)
+	{
+		scanf_s("%d", &array[i]);
+	}
 	printf("Which sort do you want to start\n");
 	printf("1.Quick sort\n2.Shell's sort\n3.Choice sort\n4.Bubble sort\n5.Piramide sort\n6.Insert sort\n");
-	scanf_s("%d",&answq);
-	switch (answq)
+	scanf_s("%d", &answq);
+	switch(answq)
 	{
-	case 1:
-		double timer = 0.0;
-		clock_t begin = clock();
-		Quicksort(array,0,12);
-		clock_t end = clock();
-		timer += (double)(end - begin) / CLOCKS_PER_SEC;
-		printf("Time elapsed %lfs",timer);
-		break;
-	case 2:
-		Shellsort();
-		break;
-	case 3:
-		Choicesort();
-		break;
-	case 4:
-		Bubblesort();
-		break;
-	case 5:
-		Piramidesort();
-		break;
-	case 6:
-		Insertsort();
-		break;
-	default:
-		printf("There is no such a sort number!");
+		case 1:
+			break;
+		case 2:
+			Shellsort();
+			break;
+		case 3:
+			Choicesort();
+			break;
+		case 4:
+			laws(array, 12);
+			break;
+		case 5:
+			Piramidesort();
+			break;
+		case 6:
+			Insertsort();
+			break;
+		default:
+			printf("There is no such a sort number!");
 	}
 }
