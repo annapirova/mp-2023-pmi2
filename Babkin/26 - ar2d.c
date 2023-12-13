210d8691dbfa443c786df5ba39863324cd69531d
Unknown <144899753+KlozenSMA@users.noreply.github.com>
2023-12-13 16:09:24 +0300

    Отправлено на переделку

--- Babkin/26 - ar2d.c
+++ Babkin/26 - ar2d.c
@@ -5,8 +5,8 @@
 int main()
 {
     int n; int *hdmatrix;int *hay;int *howmuch;
-    printf("Enter your n \n");
-    scanf("%d",&n);
+    printf_s("Enter your n \n");
+    scanf_s("%d",&n);
     printf("\n");
     hdmatrix = (int*)malloc(n*n*sizeof(int));
     for (int queo = 0;queo < n;queo++)
@@ -14,67 +14,19 @@ int main()
         for (int sint = 0;sint < n;sint++)
         {
             hdmatrix[n*queo + sint] = 1 + rand()%(n);
-            printf("%d ",hdmatrix[n*queo + sint]);
+            printf_s("%d ",hdmatrix[n*queo + sint]);
         }
-        printf("\n");
+        printf_s("\n");
     }
-    hay = (int *)malloc(n*sizeof(int));
-    for (int k = 0;k < n;k++)
+    bool cond = true;
+    hay = (int*)malloc(n*sizeof(int));
+    for (int i = 0; i < n; i++)
     {
-        hay[k] = k + 1;
-    }
-    bool what = true;
-    for (int s = 0;s<n;s++)
-    {
-        howmuch = (int*)malloc(n*sizeof(int));
-        for (int i = 0;i < n;i++)
+        for (int u = 0; u < n; u++)
         {
-            howmuch[hdmatrix[n*s + i]-1]+=1;
-        }
-        for(int q = 0;q < n; q++)
-        {
-            if (howmuch[q] != 1)
+            for (int j = 0; j < n; j++)
             {
-                what = false;
-                break;
             }
         }
-        free(howmuch);
-        if (what == false)
-        {
-            break;
-        }
-    }
-    if (what == true)
-    {
-        for (int s = 0;s<n;s++)
-        {
-            howmuch = (int*)malloc(n*sizeof(int));
-            for (int i = 0;i < n;i++)
-            {
-                howmuch[hdmatrix[n*i + s]-1]+=1;
-            }
-            for(int q = 0;q < n; q++)
-            {
-                if (howmuch[q] != 1)
-                {
-                    what = false;
-                    break;
-                }
-            }
-            free(howmuch);
-            if (what == false)
-            {
-                break;
-            }
-        }
-    }
-    if (what == true)
-    {
-        printf("You've got a LATIN SQUARE");
-    }
-    else
-    {
-        printf("It is unintresting usual matrix");
     }
 }
