2c89cc3c8b76b42f6c73dab28a83cb7c3e8c735a
Unknown <144899753+KlozenSMA@users.noreply.github.com>
2023-12-20 01:18:57 +0300

    Переделка не переделка

--- Babkin/26 - ar2d.c
+++ Babkin/26 - ar2d.c
@@ -4,29 +4,92 @@
 #include "stdbool.h"
 int main()
 {
-    int n; int *hdmatrix;int *hay;int *howmuch;
-    printf_s("Enter your n \n");
+    int *matrrix;
+    int n;
+    int *pixmassive;
+    int *whatmassive; int *etalonmassive;
+    bool mihp = true;
     scanf_s("%d",&n);
     printf("\n");
-    hdmatrix = (int*)malloc(n*n*sizeof(int));
-    for (int queo = 0;queo < n;queo++)
+    etalonmassive = (int*)malloc(n*sizeof(int));
+    for (int y = 0;y < n;y++)
     {
-        for (int sint = 0;sint < n;sint++)
-        {
-            hdmatrix[n*queo + sint] = 1 + rand()%(n);
-            printf_s("%d ",hdmatrix[n*queo + sint]);
-        }
-        printf_s("\n");
+        etalonmassive[y] = 1;
     }
-    bool cond = true;
-    hay = (int*)malloc(n*sizeof(int));
-    for (int i = 0; i < n; i++)
+    matrrix = (int*)malloc(n*n*sizeof(int));
+    for (int i = 0;i < n;i++)
     {
-        for (int u = 0; u < n; u++)
+        for (int j = 0;j < n;j++)
         {
-            for (int j = 0; j < n; j++)
+            matrrix[n*i+j] = 1 + rand() % (n);
+            printf("%d ",matrrix[n*i+j]);
+        }
+        printf("\n");
+    }
+    pixmassive = (int*)malloc(n*sizeof(int));
+    for (int k = 1;k<n+1;k++)
+    {
+        pixmassive[k-1] = k;
+    }
+    for (int i = 0;i < n;i++)
+    {
+        whatmassive = (int*)malloc(n*sizeof(int));
+        for (int j = 0; j < n;j++)
+        {
+            for (int s = 0; s < n; s++)
             {
+                if (matrrix[n*i + j] == pixmassive[s])
+                {
+                    whatmassive[s] += 1;
+                }
             }
         }
+        for (int p = 0;p<n;p++)
+        {
+            if (etalonmassive[p] != whatmassive[p])
+            {
+                mihp = false;
+                break;
+            }
+        }
+        if (mihp == false)
+        {break;}
+        free(whatmassive);
+    }
+    if (mihp == true)
+    {
+        for (int j = 0;j < n;j++)
+        {
+            whatmassive = (int*)malloc(n*sizeof(int));
+            for (int i = 0; i < n;i++)
+            {
+                for (int s = 0; s < n; s++)
+                {
+                    if (matrrix[n*i + j] == pixmassive[s])
+                    {
+                        whatmassive[s] += 1;
+                    }
+                }
+            }
+            for (int p = 0;p<n;p++)
+            {
+                if (etalonmassive[p] != whatmassive[p])
+                {
+                    mihp = false;
+                    break;
+                }
+            }
+            if (mihp == false)
+            {break;}
+            free(whatmassive);
+        }
+    }
+    if (mihp == true)
+    {
+        printf("LATIN MATRIX");
+    }
+    if (mihp == false)
+    {
+        printf("NOT LATIN MATRIX");
     }
 }
