dea14dd559f122140190bae5ea36a611efa9e70f
Roman Krotov <144903086+Rabramxv@users.noreply.github.com>
2023-12-27 14:13:12 +0300

    .

---
+++ Massiv6.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int sumElementsWithShift(int matrix[][100], int n, int k) {
+    int sum = 0;
+    int i, j;
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (j == i + k || j == i - k) {
+                sum += matrix[i][j];
+            }
+        }
+    }
+
+    return sum;
+}
+
+int main() {
+    setlocale(LC_CTYPE, "Russian");
+    int matrix[100][100];
+    int n, k;
+    int i, j;
+
+    printf("Vvedi razmer matrici (N): ");
+    scanf_s("%d", &n);
+
+    printf("Znachenie sdviga(k): ");
+    scanf_s("%d", &k);
+
+    printf("Введите элементы матрицы:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf_s("%d", &matrix[i][j]);
+        }
+    }
+
+    int sum = sumElementsWithShift(matrix, n, k);
+
+    printf("Сумма элементов со сдвигом %d от диагонали матрицы: %d\n", k, sum);
+
+    return 0;
+}
---
+++ massiv18.c
@@ -0,0 +1,63 @@
+﻿#include <stdio.h>
+#define MAX_ROWS 100
+#define MAX_COLS 100
+void findSaddlePoints(int matrix[MAX_ROWS][MAX_COLS], int rows, int cols, int saddlePoints[MAX_ROWS * MAX_COLS][2], int* numSaddlePoints)
+{
+    *numSaddlePoints = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        int minRow = matrix[i][0];
+        int colIndex = 0; // Находим минимальное значение в строке и запоминаем индекс столбца
+        for (int j = 1; j < cols; j++)
+        {
+            if (matrix[i][j] < minRow)
+            {
+                minRow = matrix[i][j];
+                colIndex = j;
+            }
+        }
+        // Проверяем, является ли минимальное значение в строке максимальным в столбце
+        int isSaddlePoint = 1;
+        for (int k = 0; k < rows; k++)
+        {
+            if (matrix[k][colIndex] > minRow)
+            {
+                isSaddlePoint = 0;
+                break;
+            }
+        }
+        // Если является, добавляем индексы в массив седловых точек
+        if (isSaddlePoint)
+        {
+            saddlePoints[*numSaddlePoints][0] = i;
+            saddlePoints[*numSaddlePoints][1] = colIndex;
+            (*numSaddlePoints)++;
+        }
+    }
+}
+int main()
+{
+    int matrix[MAX_ROWS][MAX_COLS];
+    int rows, cols;
+    printf("Vvedite count strok: ");
+    scanf_s("%d", &rows);
+    printf("Vvedite count stolbcov: ");
+    scanf_s("%d", &cols);
+    printf("vvedi element matrici:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf_s("%d", &matrix[i][j]);
+        }
+    }
+    int saddlePoints[MAX_ROWS * MAX_COLS][2];
+    int numSaddlePoints;
+    findSaddlePoints(matrix, rows, cols, saddlePoints, &numSaddlePoints);
+    printf("sedlovie tochki:\n");
+    for (int i = 0; i < numSaddlePoints; i++)
+    {
+        printf("(%d, %d)\n", saddlePoints[i][0], saddlePoints[i][1]);
+    }
+    return 0;
+}
