58f468e1c84a7add55e3f94ed6979b8d97bec15f
Unknown <144899753+KlozenSMA@users.noreply.github.com>
2024-03-04 23:28:56 +0300

    Update FileName.cpp
    Я... Я не знаю

--- Babkin/FileName.cpp
+++ Babkin/FileName.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include "stdio.h"
 #include "trian_gh.h"
 using namespace std;
 int main()
 {
-	trian_gh g(10, 4, 5), g2(g);
+	int choice;
+	trian_gh g(10, 4, 5);
 	double x = g.S();
-	cout << x;
+	double y = g.p();
+	printf("Choose an option to know about Perimeter(1) or Area(2)");
+	printf("\n");
+	scanf("%d",&choice);
+	printf("\n");
+	switch (choice)
+	{
+		case(1): printf("%d\n",y);
+		case(2): printf("%d\n",x);
+		default: printf("sorry no matches to your search");
+	}
 }
