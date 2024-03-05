788baf7972335c85ff5c402f74cd64f0b4bd64eb
Unknown <144899753+KlozenSMA@users.noreply.github.com>
2024-03-05 13:55:07 +0300

    Update FileName.cpp
    Переделан полностью под c++. Исправлена ошибка со switch.

--- Babkin/FileName.cpp
+++ Babkin/FileName.cpp
@@ -1,20 +1,20 @@
-#include "stdio.h"
+#include <iostream>
 #include "trian_gh.h"
 using namespace std;
 int main()
 {
 	int choice;
-	trian_gh g(10, 4, 5);
+	trian_gh g(1.7, 6.8, 9.1);
 	double x = g.S();
 	double y = g.p();
-	printf("Choose an option to know about Perimeter(1) or Area(2)");
-	printf("\n");
-	scanf("%d",&choice);
-	printf("\n");
+	cout << "Choose an option to know about Perimeter(1) or Area(2)";
+	cout << "\n";
+	cin >> choice;
+	cout << "\n";
 	switch (choice)
 	{
-		case(1): printf("%d\n",y);
-		case(2): printf("%d\n",x);
-		default: printf("sorry no matches to your search");
+		case(1): cout << y; break;
+		case(2): cout << x; break;
+		default: cout << "sorry no matches to your search";
 	}
 }
