a7b5734556f3dfaf7eb8038dfef47d297a54888d
arasash1 <zhdankin228@gmail.com>
2024-03-26 02:43:16 +0300

    Жданкин Матрицы и Векторы

---
+++ Zhdankin/Vector/Vector.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef VECTOR__
+#define VECTOR__
+#include <iostream>
+using namespace std;
+
+class Vector
+{
+	int n;
+	double* vec;
+public:
+	Vector(int n);
+	Vector(const Vector& C);
+	~Vector();
+
+	void Generate(double a, double b);
+
+	friend ostream& operator<<(ostream& osin, const Vector& C);
+	friend istream& operator>>(istream& isin, const Vector& C);
+
+	Vector& operator=(const Vector& C);
+	Vector operator+(const Vector& C);
+	Vector operator-(const Vector& C);
+	Vector& operator+=(const Vector& C);
+	Vector& operator-=(const Vector& C);
+	double& operator[](const int i);
+	Vector operator++(int);
+	Vector& operator++();
+	Vector operator*(const Vector& C);
+};
+
+#endif
