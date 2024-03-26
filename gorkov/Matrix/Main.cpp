7f2f346800b8849aa979dbaff11e55100d902d4e
Илья Горьков <144900623+Ilreddem@users.noreply.github.com>
2024-03-26 14:12:33 +0300

    Update Main.cpp

--- gorkov/Matrix/Main.cpp
+++ gorkov/Matrix/Main.cpp
@@ -1,32 +1,39 @@
 #include "matrix.h"
 #include <iostream>
-#include <cstdio>
+
 using namespace std;
 
 int main()
 {
-	Matrix K(3, 3), B(3, 5), D(1, 2);
-	int min, max;
 	cout << "Enter min and max of numbers" << endl;
+	Matrix K(2, 2), B(2, 2), D(1, 2);
+	int min, max;
 	cin >> min >> max;
 	B.Generate(min, max);
 	//D.Print();
 	K.Generate(min, max);
-	Matrix C(B);
-	//B.Print();
-	//K.Print();
+	Matrix C(B); 
+	
+	B.Print();
+	cout << endl;
+	K.Print();
+	cout << endl;
 	K = K * B;
 	B += B;
-	B.Null();
-	//B.Print();
-	//K.Print();
+	//B.Null();
+	B.Print();
+	K.Print();
 	
-	Vector T(3),Y, L;
+	Vector T(3),Y, L, x;
 	T.Genetate(min, max);
+	//cin >> T;
 	Y = T;
 	Y.Print();
 	L = (Y + T);
 	L.Print();
-	
+	L[0] = Y[1] * 5;
+	++L;
+	cout << L;
+
 	return 0;
 }
