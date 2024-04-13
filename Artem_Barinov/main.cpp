559950efe07e28336a2164cc04690a298973155d
Artem Barinov <145492721+Aigo-Oggi@users.noreply.github.com>
2024-04-13 20:32:38 +0300

    Гаусс итоговый (?)

--- Artem_Barinov/main.cpp
+++ Artem_Barinov/main.cpp
@@ -1,10 +1,19 @@
-#include <iostream>
-#include "matrix.cpp"
-Matrix::Matrix(const Matrix& B) {
-	A = new double* [n];
+#include "MVecClass.h"
 
-}
 int main() {
+	Matrix matrix, smatr;
+	std::cin >> matrix; smatr = matrix;
+	std::cout << matrix << std::endl;
 
+	Vector vect(3);
+	std::cin >> vect;
+	Vector sv(vect);
+
+	matrix = Gaus(matrix, vect);
+	std::cout << matrix << std::endl;
+	std::cout << vect << std::endl;
+	ReGaus(matrix, vect);
+
+	GSolCheck(smatr, vect, sv);
 	return 0;
 }
