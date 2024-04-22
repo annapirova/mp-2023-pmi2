c09461882549694843d3a8f54f6bde477565274e
Unknown <144899753+KlozenSMA@users.noreply.github.com>
2024-04-22 23:53:22 +0300

    Загрузка матриц на сервер

--- Babkin/matrix.h
+++ Babkin/matrix.h
@@ -1,79 +1,115 @@
 #pragma once
 #include <iostream>
+#include "vector.h"
 using namespace std;
-class vector
+class matrix
 {
 	private:
-	int size; 
-	double* vect;
+	int size,sizex; 
+	vector* matr;
 	public:
-		vector(int _size = 3) { 
+		matrix(int _size = 3,int _sizex = 4) { 
 			size = _size; 
-			vect = new double[size];
+			sizex = _sizex;
+			matr = new vector[size];
+			for (int u = 0; u < size; u++)
+			{
+				matr[u] = vector(sizex);
+			}
 		}
-		~vector()
+		~matrix()
 		{
-			delete[] vect;
+			delete[] matr;
 		}
-		vector(const vector &othl)
+		matrix(const matrix &othl)
 		{
 			size = othl.size;
-			vect = new double[size];
-			memcpy(vect, othl.vect, sizeof(double) * size);
+			sizex = othl.sizex;
+			matr = new vector[size];
+			for (int u = 0; u < size; u++)
+			{
+				matr[u] = vector(sizex);
+			}
+			memcpy(matr, othl.matr, sizeof(vector)* sizex);
 		}
-		double& operator[](const int i)
+		double& operator()(const int i,const int x)
 		{
-			return vect[i];
+			return matr[i][x];
 		}
-		const double& operator[] (const int i) const
+		const double& operator() (const int i,const int x) const
 		{
-			return vect[i];
+			return matr[i][x];
 		}
-		vector& operator=(const vector& p)
+		matrix& operator=(const matrix& p)
 
 		{
 			if (this != &p)
 			{
 				size = p.size;
-				vect = new double[size];
+				sizex = p.sizex;
+				matr = new vector(size);
+				for (int u = 0; u < size; u++)
+				{
+					matr[u] = vector(sizex);
+				}
 			}
-			memcpy(vect, p.vect, sizeof(double) * size);
+			memcpy(matr, p.matr, sizeof(vector) * sizex);
 			return *this;
 		}
-		vector& operator+=(const vector& y)
+		matrix& operator+=(const matrix& y)
 		{
 			for (int u = 0; u < size; u++)
 			{
-				vect[u] += y.vect[u];
+				for (int h = 0;h < sizex;h++)
+				{
+					matr[u][h] += y.matr[u][h];
+				}
 			}
 			return *this;
 		}
-		vector& operator-=(const vector& y)
+		matrix& operator-=(const matrix& y)
 		{
 			for (int u = 0; u < size; u++)
 			{
-				vect[u] -= y.vect[u];
+				for (int h = 0;h < sizex;h++)
+				{
+					matr[u][h] -= y.matr[u][h];
+				}
 			}
 			return *this;
 		}
-		vector& operator*=(double& y)
+		matrix& operator*=(double& y)
 		{
 			for (int u = 0; u < size; u++)
 			{
-				vect[u] *= y;
+				for (int h = 0; h<sizex;h++)
+				{
+					matr[u][h] *= y;
+				}
 			}
 			return *this;
 		}
-		friend ostream& operator<<(ostream& vout,const vector& Imma)
+		friend ostream& operator<<(ostream& vout,const matrix& Imma)
 		{
 			for (int i = 0; i < Imma.size; i ++)
-			{vout << Imma.vect[i];}
+			{
+				for (int u = 0; u < Imma.sizex; u++)
+				{
+					vout << Imma.matr[i][u] << " ";
+				}
+				vout << "\n";
+			}
 			return vout;
 		}
-		friend istream& operator>>(istream& vin,const vector& Imma)
+		friend istream& operator>>(istream& vin,const matrix& Imma)
 		{
-			for (int i = 0; i < Imma.size; i ++)
-			{vin >> Imma.vect[i];}
+			for (int i = 0; i < Imma.size; i++)
+			{
+				for (int ox = 0; ox < Imma.sizex; ox++)
+				{
+					vin >> Imma.matr[i][ox];
+				}
+			}
 			return vin;
 		}
 };
