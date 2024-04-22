29b1b33c1c212bac9795f77b87adcafa2fc7069e
Anna Pirova <anna.yu.malova@gmail.com>
2024-04-23 01:34:44 +0300

    пример наследование

---
+++ pirova_examples/Transport/main.cpp
@@ -0,0 +1,34 @@
+#include "transport.h"
+#include "landTransport.h"
+#include "airTransport.h"
+#include <iostream>
+
+const int n = 5;
+
+void main()
+{
+  Transport man("man", 5.0, 1), tortoise("tortoise", 0.1, 0, RED);
+  man.Info();
+
+  LandTransport car("car", 60.0, 4, BLUE, WEELS, 4), car2(car), snowmobile("snowmobile");
+  car.Transport::Info();
+  car.Info();
+  snowmobile.Info();
+
+  AirTransport plane("plane", 800.0, 120, WHITE, CIVILIAN, 4),
+    helicopter("hel", 300.0, 4, GREEN, MILITARY, 1);
+
+  Transport** transpTable = new Transport* [n];
+  transpTable[0] = &plane;
+  transpTable[1] = &helicopter;
+  transpTable[2] = &car;
+  transpTable[3] = &car2;
+  transpTable[4] = &snowmobile;
+
+  std::cout << "****************\n";
+  for(int i = 0; i < n; i++)
+    transpTable[i]->Info();
+  std::cout << "****************\n";
+
+  delete[] transpTable;
+}
---
+++ pirova_examples/Transport2/transport.h
@@ -0,0 +1,26 @@
+#ifndef _TRANSPORT_H_
+#define _TRANSPORT_H_
+
+#include <string>
+
+typedef enum {RED, GREEN, BLUE, WHITE, BLACK} ColorType;
+
+class Transport
+{
+private:
+  double velocity;
+protected:
+  ColorType color;
+public:
+  int nPassengers;
+  std::string name;
+  
+  Transport(std::string name_, double v = 0.0, int nP = 0, ColorType col = BLACK);
+  Transport(std::string name_, int v, int nP, ColorType col);
+  Transport(const Transport& tr);
+  virtual ~Transport(); // рекомендуется
+  virtual void Info();
+  virtual void Go() = 0;
+};
+
+#endif
