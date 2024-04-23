f2c2292ee4115b057fb49c22aafff412ad8e5908
Unknown <144899753+KlozenSMA@users.noreply.github.com>
2024-04-23 14:02:32 +0300

    Update vector.h

--- Babkin/vector.h
+++ Babkin/vector.h
@@ -3,77 +3,90 @@
 using namespace std;
 class vector
 {
-	private:
-	int size; 
+private:
+	int size;
 	double* vect;
-	public:
-		vector(int _size = 3) { 
-			size = _size; 
-			vect = new double[size];
-		}
-		~vector()
-		{
-			delete[] vect;
-		}
-		vector(const vector &othl)
-		{
-			size = othl.size;
-			vect = new double[size];
-			memcpy(vect, othl.vect, sizeof(double) * size);
-		}
-		double& operator[](const int i)
-		{
-			return vect[i];
-		}
-		const double& operator[] (const int i) const
-		{
-			return vect[i];
-		}
-		vector& operator=(const vector& p)
+public:
+	vector(int _size = 3) {
+		size = _size;
+		vect = new double[size];
+	}
+	~vector()
+	{
+		delete[] vect;
+	}
+	vector(const vector& othl)
+	{
+		size = othl.size;
+		vect = new double[size];
+		memcpy(vect, othl.vect, sizeof(double) * size);
+	}
+	double& operator[](const int i)
+	{
+		return vect[i];
+	}
+	const double& operator[] (const int i) const
+	{
+		return vect[i];
+	}
+	vector& operator=(const vector& p)
 
+	{
+		if (this != &p)
 		{
-			if (this != &p)
-			{
-				size = p.size;
-				vect = new double[size];
-			}
-			memcpy(vect, p.vect, sizeof(double) * size);
-			return *this;
+			size = p.size;
+			vect = new double[size];
 		}
-		vector& operator+=(const vector& y)
+		memcpy(vect, p.vect, sizeof(double) * size);
+		return *this;
+	}
+	vector& operator+=(const vector& y)
+	{
+		for (int u = 0; u < size; u++)
 		{
-			for (int u = 0; u < size; u++)
-			{
-				vect[u] += y.vect[u];
-			}
-			return *this;
+			vect[u] += y.vect[u];
 		}
-		vector& operator-=(const vector& y)
+		return *this;
+	}
+	vector& operator-=(const vector& y)
+	{
+		for (int u = 0; u < size; u++)
 		{
-			for (int u = 0; u < size; u++)
-			{
-				vect[u] -= y.vect[u];
-			}
-			return *this;
+			vect[u] -= y.vect[u];
 		}
-		vector& operator*=(double& y)
+		return *this;
+	}
+	vector& operator*=(double& y)
+	{
+		for (int u = 0; u < size; u++)
 		{
-			for (int u = 0; u < size; u++)
-			{
-				vect[u] *= y;
-			}
-			return *this;
+			vect[u] *= y;
 		}
-		friend ostream& operator<<(ostream& vout,const vector& Imma)
+		return *this;
+	}
+	friend ostream& operator<<(ostream& vout, const vector& Imma)
+	{
+		for (int i = 0; i < Imma.size; i++)
 		{
-			for (int i = 0; i < Imma.size; i ++)
-			{vout << Imma.vect[i] << " ";}
-			return vout;
+			vout << Imma.vect[i] << " ";
 		}
-		friend istream& operator>>(istream& vin,const vector& Imma)
+		return vout;
+	}
+	friend istream& operator>>(istream& vin, const vector& Imma)
+	{
+		for (int i = 0; i < Imma.size; i++)
 		{
-			for (int i = 0; i < Imma.size; i ++)
-			{vin >> Imma.vect[i];}
-			return vin;
+			vin >> Imma.vect[i];
 		}
+		return vin;
+	}
+	double younormie(const vector& t)
+	{
+		double commence = 0;
+		for (int j = 0; j < t.size; j++)
+		{
+			commence += abs(t[j]);
+		}
+		return commence;
+	}
 };
